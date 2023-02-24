10f0c1e3a84b8831d773de3c5523bc899816937f
AlekseyMegliy <meglij46@gmail.com>
2023-02-24 10:18:08 +0200

    slider bug fix

--- src/components/ui/slider/SliderComponent.tsx
+++ src/components/ui/slider/SliderComponent.tsx
@@ -8,27 +8,22 @@ export default function SliderComponent(props: SliderComponentProps) {
   const [shift, setShift] = useState<number>(0);
   const [rightEdge, setEdge] = useState<boolean>(false);
   const [width, setWidth] = useState<number>(window.innerWidth);
-  const max: number = 10;
+  const max: number = 8;
 
   useEffect(() => {
     window.addEventListener("resize", updateWidth);
   }, []);
 
   function right() {
-    if (width > 991 && Math.floor(max / 3) > shift / -100 + 1) {
+    if (width > 991 && Math.floor(max / 3) > Math.round(shift) / -100 + 1) {
       setShift(shift - 100);
-      console.log("shift / -100 + 1 " + shift / -100 + 1);
-      console.log("Math.floor(max / 3) " + Math.floor(max / 3));
     } else if (
       width > 991 &&
       Math.floor(max / 3) === shift / -100 + 1 &&
       max % 3 === 2
     ) {
-      setShift(shift - 66.6666);
+      setShift(shift - 66.6665);
       setEdge(true);
-      console.log("width: " + width);
-      console.log("shift / -100 + 1: " + shift / -100 + 1);
-      console.log("Math.floor(max / 3): " + Math.floor(max / 3));
     } else if (
       width > 991 &&
       Math.floor(max / 3) === shift / -100 + 1 &&
@@ -45,11 +40,11 @@ export default function SliderComponent(props: SliderComponentProps) {
   }
 
   function left() {
-    if (width > 991 && shift % -100 === 0 && shift !== 0) {
+    if (width > 991 && Math.round(shift) % -100 === 0 && shift !== 0) {
       setShift(shift + 100);
       setEdge(false);
     } else if (width > 991 && shift % -100 !== 0 && max % 3 === 2) {
-      setShift(shift + 66.6666);
+      setShift(shift + 66.6665);
       setEdge(false);
     } else if (width > 991 && shift % -100 !== 0 && max % 3 === 1) {
       setShift(shift + 33.3333);
@@ -70,7 +65,7 @@ export default function SliderComponent(props: SliderComponentProps) {
       <span
         className="slider__left-button"
         onClick={left}
-        style={shift === 0 ? { display: "none" } : undefined}
+        style={Math.round(shift) === 0 ? { display: "none" } : undefined}
       >
         <img alt="slider__arrow" className="slider__arrow" src={arrow} />
       </span>
@@ -80,7 +75,9 @@ export default function SliderComponent(props: SliderComponentProps) {
           <div
             key={dogy.id}
             className="col-12 col-lg-4 products-row__card"
-            style={{ left: shift + "%" }}
+            style={
+              index > max - 1 ? { display: "none" } : { left: shift + "%" }
+            }
           >
             <Product dogy={dogy} limit={max} />
           </div>
