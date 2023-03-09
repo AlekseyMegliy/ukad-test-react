9af2d5763b08c2a01601c12b561a0914e4132385
AlekseyMegliy <meglij46@gmail.com>
2023-03-09 18:01:12 +0200

    Restarting the product pages after switching from home

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -4,8 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import SliderComponent from "../../components/ui/slider/Slider";
 import Loader from "../../components/ui/loader/Loader";
 import { BreedType } from "../../types";
+import { useAppDispatch } from "../../shared/utils";
+import { changePage } from "../../components/ui/redux-paginstion-slice/fetch-slice";
 
 function Home() {
+  const dispatch = useAppDispatch();
   const [breed, setBreed] = useState<BreedType[]>([]);
   useEffect(() => {
     fetch("https://api.thedogapi.com/v1/breeds?limit=10&page=0")
@@ -14,7 +17,10 @@ function Home() {
       .catch((error) => {
         console.error("Error fetching breed information:", error);
       });
-  }, [breed]);
+    return () => {
+      dispatch(changePage(1));
+    };
+  }, [breed, dispatch]);
 
   return (
     <div className="home">
